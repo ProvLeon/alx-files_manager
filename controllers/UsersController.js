f75ff7f6d717674e7a18d2567d897d9e971b82a9
Hovixen <orevaerijo@gmail.com>
2024-07-01 19:29:51 +0000

    Added the AuthController and updated the UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,23 +1,42 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import { ObjectId } from 'mongodb';
 
-const UsersController = {
-  async postNew(req, res) {
-    const { email, password } = req.body;
+const postNew = async (req, res) => {
+  const { email, password } = req.body;
 
-    if (!email) return res.status(400).send({ error: 'Missing email' });
+  if (!email) return res.status(400).send({ error: 'Missing email' });
 
-    if (!password) return res.status(400).send({ error: 'Missing password' });
+  if (!password) return res.status(400).send({ error: 'Missing password' });
 
-    const existingUser = await dbClient.db.collection('users').findOne({ email });
+  const existingUser = await dbClient.db.collection('users').findOne({ email });
 
-    if (existingUser) return res.status(400).send({ error: 'Already exist' });
+  if (existingUser) return res.status(400).send({ error: 'Already exist' });
 
-    const hashedPassword = sha1(password);
-    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+  const hashedPassword = sha1(password);
+  const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
 
-    return res.status(201).json({ id: result.insertedId, email });
-  }
+  return res.status(201).json({ id: result.insertedId, email });
 }
 
-export default UsersController;
+const getMe = async (req, res) => {
+  const token = req.headers['x-token'];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const user = dbClient.db.collection('users').findOne({ _id: new ObjectId(userId)});
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  return res.status(200).json({ id: user._id, email: user.email });
+}
+
+module.exports = { postNew, getMe };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const routes = express.Router();
 
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
 routes.get('/users', UsersController.postNew);
+routes.get('/connect', AuthController.getConnect);
+routes.get('/disconnect', AuthController.getDisconnect);
+routes.get('/user/me', UsersController.getMe);
 
 export default routes;
