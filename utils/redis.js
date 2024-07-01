82c8e1d1a70a19b95719e155e112563d4046a005
ProvLeon <providence.leonard@gmail.com>
2024-07-01 08:04:14 +0000

    updated the DBClient in redis.js to make use of the topology.isConnected method

--- utils/redis.js
+++ utils/redis.js
@@ -1,21 +1,20 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || "localhost";
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || "files_manager";
+    const database = process.env.DB_DATABASE || 'files_manager';
 
     const url = `mongodb://${host}:${port}`;
     this.client = new MongoClient(url, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     this.client.connect((err) => {
       if (err) {
-        console.error("MongoDB Client Error:", err);
+        console.error('MongoDB Client Error:', err);
       } else {
-        console.log("MongoDB client connected");
+        console.log('MongoDB client connected');
         this.db = this.client.db(database);
       }
     });
@@ -23,24 +22,24 @@ class DBClient {
 
   isAlive() {
     return (
-      this.client && this.client.topology && this.client.topology.isConnected()
+      this.client.topology.isConnected()
     );
   }
 
   async nbUsers() {
     try {
-      return await this.db.collection("users").countDocuments();
+      return await this.db.collection('users').countDocuments();
     } catch (err) {
-      console.error("Error counting users:", err);
+      console.error('Error counting users:', err);
       throw err;
     }
   }
 
   async nbFiles() {
     try {
-      return await this.db.collection("files").countDocuments();
+      return await this.db.collection('files').countDocuments();
     } catch (err) {
-      console.error("Error counting files:", err);
+      console.error('Error counting files:', err);
       throw err;
     }
   }
