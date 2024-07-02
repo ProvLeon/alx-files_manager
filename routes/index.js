3e1272bea5d16bebdd29adecdd4ad820a802b751
Hovixen <orevaerijo@gmail.com>
2024-07-02 03:47:59 +0000

    updated the route path

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ routes.get('/stats', AppController.getStats);
 routes.post('/users', UsersController.postNew);
 routes.get('/connect', AuthController.getConnect);
 routes.get('/disconnect', AuthController.getDisconnect);
-routes.get('/user/me', UsersController.getMe);
+routes.get('/users/me', UsersController.getMe);
 
 export default routes;
